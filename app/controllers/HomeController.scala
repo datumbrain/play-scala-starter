3519b6ed7cdec9a28708768d96bd59127cdc4212
Fahad <fsdqui@gmail.com>
2019-12-18 23:37:28 +0500

    Initial Commit
    - Includes Play 2.8.x
    - Removed all comments from play-scala-starter-example from playframework's repository
    - Added .scalafmt
    - Added .gitignore
    - Fixed basic tests

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder)
+    extends AbstractController(cc) {
+  def index = Action {
+    Ok("Perfect!")
+  }
+}
